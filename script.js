5fce70a5f81ca27d6e59b817e3898ac753ab12a6
Jacob Beck <jacobbeck2008@gmail.com>
2022-09-01 19:56:02 -0500

    submit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", function () {
 
     const form = document.querySelector("form")
-    form.addEventListener("submit", function(event){
+    form.addEventListener("submit", (event)=>{
         event.preventDefault();
         let pilot = document.querySelector("input[name=pilotName]").value;
         let copilot = document.querySelector("input[name=copilotName]").value;
